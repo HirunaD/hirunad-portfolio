b72a95651fee58c04c7a4766d34e6f10cd25aa43
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2026-07-05 01:37:44 +0530

    feat: change title

--- app/src/data/navigation.ts
+++ app/src/data/navigation.ts
@@ -3,4 +3,5 @@ export const navLinks = [
   { name: "Projects", href: "#projects" },
   { name: "Skills", href: "#skills" },
   { name: "Articles", href: "#articles" },
+  { name: "Contact", href: "#contact" },
 ];
